30e471f5af60ec729bd10a26af020363d2ef2021
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-12-13 18:40:26 +0300

    Added solution for Subdomain Visit Count

---
+++ A2SV-Group-5/subdomain-visit-count.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        names =defaultdict(int)
+        number=0
+        text=""
+
+        for i in range (len(cpdomains)):
+            number,text=cpdomains[i].split()
+            # print(number)
+            number=int(number)
+            names[text]+=number
+            text=text.split(".")
+            
+            for i in range(1,len(text)):
+                names[".".join(text[i:])]+=number
+        
+        ans=[]
+        for key , value in names.items():
+            ans.append(str(value)+" "+key)
+            
+        return ans
