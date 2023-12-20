8f11dc36dbe058cf3a93ee2ed72546cbb195ba5e
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-12-20 10:27:38 +0300

    Added solution for Card Flipping Game

---
+++ A2SV-Group-5/card-flipping-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flipgame(self, fronts, backs):
+        """
+        :type fronts: List[int]
+        :type backs: List[int]
+        :rtype: int
+        """
+        s = set()
+        res = float('inf')
+        for f, b in zip(fronts, backs):
+            if f == b:
+                s.add(f)
+        for f in fronts:
+            if f not in s:
+                res = min(res, f)
+        for b in backs:
+            if b not in s:
+                res = min(res, b)
+        return 0 if res == float('inf') else res
