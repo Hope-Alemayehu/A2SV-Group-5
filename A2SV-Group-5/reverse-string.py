0609baeda8b08588138466723883813b67e33bd3
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-12-22 17:58:36 +0300

    Added solution for Reverse String

--- A2SV-Group-5/reverse-string.py
+++ A2SV-Group-5/reverse-string.py
@@ -1,16 +1,12 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        # left = 0
-        # right = len(s) - 1
-        # while left < right:
-        #     s[left], s[right] = s[right], s[left]
-        #     left += 1
-        #     right -= 1
-        # return s
-        self.reverseHelper(s,0,len(s)-1)
-    def reverseHelper(self,s:List[str],left:int,right:int):
-        if left>=right:
-            return s
-        
-        s[left],s[right]=s[right],s[left]
-        self.reverseHelper(s,left+1,right-1)
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left=0
+        right=len(s)-1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        return s        
