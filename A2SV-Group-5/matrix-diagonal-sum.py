bea92b36f8c92a7bdd4c8293901caa893abd8b21
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-12-25 19:47:38 +0300

    Added solution for Matrix Diagonal Sum

---
+++ A2SV-Group-5/matrix-diagonal-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ans=0
+        
+        n=len(mat)
+        for i in range (len(mat)):
+            for j in range(len(mat[0])):
+                if i==j:
+                    ans+=mat[i][j]
+                elif i+j == n-1 :
+                    ans+=mat[i][j]
+       
+        
+        return ans
+            
+    
+
+            
