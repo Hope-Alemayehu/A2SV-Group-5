6f5a09f32e5e0da84bfda5ca18ee81cb994a050d
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-09-02 10:29:36 +0300

    Time: 88 ms (65.6%), Space: 20.4 MB (45.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        check = defaultdict(list)
-        for st in strs:
-            c = sorted(st)
-            check[str(c)].append(st)
+        #time complexity
+        #space complexity
 
-        return list(check.values())
+        counter = {}
+
+        for s in strs:
+            sortedStr = sorted(s)
+            sortedStr = "".join(sortedStr)
+            #making sure the key exists
+            if sortedStr not in counter:
+                counter[sortedStr] = []
+            counter[sortedStr].append(s)
+        res = []
+        for key, val in counter.items():
+            res.append(list(val))
+        return res
+
