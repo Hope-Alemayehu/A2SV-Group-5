3f4cd00219077cf3b35db2fd2520a180abb020a9
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-09-05 14:51:16 +0300

    Time: 678 ms (69.14%), Space: 23.8 MB (73.39%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -1,8 +1,11 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        # Check if reshaping is possible
-        if len(original) != m * n:
-            return [] # Return an empty array if not possible
-        
-        # Fill the 2D array by slicing the original list
-        return [original[i * n:(i + 1) * n] for i in range(m)]
+        if m * n != len(original):
+            return []
+
+        ans = []
+        k = 0
+        while k < len(original):
+            ans.append(original[k:k+n])
+            k += n
+        return ans
