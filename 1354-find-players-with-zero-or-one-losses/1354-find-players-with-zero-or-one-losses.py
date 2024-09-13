e415aefb0b9a4050ab2427c1daf58c95b8679e04
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-09-13 05:05:02 +0300

    Time: 1388 ms (72.23%), Space: 70.4 MB (98.51%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
@@ -1,5 +1,7 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        #time complexity O(2N)
+        #space complexity O(N)
         hashi =  defaultdict(int)
         for x, y in matches:
             hashi[y] += 1
@@ -11,9 +13,4 @@ class Solution:
                 list1.add(x)
             if hashi[y] == 1:
                 list2.add(y)
-        list1 = list(list1)
-        list1.sort()
-        list2 = list(list2)
-        list2.sort()
-        ans = [list1,list2]
-        return ans
+        return [sorted(list(list1)), sorted(list(list2))]
