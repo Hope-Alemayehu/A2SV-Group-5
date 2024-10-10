985c95ad7da285061333824c162b610bd3f91d37
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-10-10 18:16:43 +0300

    Time: 556 ms (5.85%), Space: 18.6 MB (42.06%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,24 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        
+        #usig bellman ford algoritsm (pull DP)
+        #with the space optimized
         graph = defaultdict(list)
-
-        for start, end, weight in times:
-            graph[start].append((end, weight))
+        for u, v, w in times:
+            #we are storing which node are pointing to V
+            graph[v-1].append((u-1, w))
         
+        prev = [float("inf") for _ in range(n)] 
+        #our start node isn't found at index 0 instead k-1
+        prev[k-1] = 0
 
-        distance = {node: float("inf") for node in range(1,n+1)}
-        distance[k] = 0
+        for i in range (n-1):
+            curr = prev[:]
+            for j in range(n):
+                for nei, weight in graph[j]:
+                    #relaxation happens here
+                    if weight + curr[nei] < curr[j]:
+                        curr[j] = weight + curr[nei]
+                    
+                prev = curr
+        # print(prev)
+        res = max(prev)
+        return res if res != float('inf') else -1
 
-        heap = [(0, k)]
-        while heap:
-
-            node_val, node = heap.pop()
-
-            for neighbour, weight in graph[node]:
-                if node_val + weight < distance[neighbour]:
-                    distance[neighbour] = node_val + weight
-                    heapq.heappush(heap,(distance[neighbour], neighbour))
-            
-        ans = max(distance.values())
-        return ans if ans != float("inf") else -1
