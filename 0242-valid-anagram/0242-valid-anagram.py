28f59dd8751ba1fa3577a07c0c8fd4d1e4d34a03
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-05 20:50:55 +0300

    Time: 15 ms (49.85%), Space: 16.8 MB (84.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        #we can sort them and check if they are equal
-        #time complexity would be O(NlogN) however we can do this in O(N)
-
-        return Counter(s) == Counter(t)
+        #get the count of each character for s
+        #iterate through the string of t while minimizing the frequency of t
+        #check if the dictionary is empty in the end
+        if len(s) != len(t):
+            return False
+        freq = {}
+        for c in t:
+            freq[c] = freq.get(c,0) + 1
+        
+        for c in s:
+            if c in freq:
+                freq[c] -= 1
+                if freq[c] == 0:
+                    del(freq[c])
+        # print(freq)
+        return len(freq) == 0
