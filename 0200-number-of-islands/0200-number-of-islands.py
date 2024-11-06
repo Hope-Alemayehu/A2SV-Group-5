9b994f40e6030ba94b049c2429d450e4923853f5
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-06 14:08:12 +0300

    Time: 235 ms (69.94%), Space: 19 MB (71.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-       
-        directions = [[0,1],[0,-1],[-1,0],[1,0]]
+        #define the four directions
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         rows, cols = len(grid), len(grid[0])
-
-        def dfs(r,c):
-            if r == rows or r < 0 or c == cols or c < 0 or grid[r][c] != "1":
+        #define a dfs function
+        def dfs(r: int,c: int) -> None:
+            if r < 0 or r == rows or c < 0 or c == cols or grid[r][c] != "1":
                 return
-           
-            grid[r][c] = -1
+          
+            grid[r][c] = "2"
+
             for x, y in directions:
-                rn = r + x
-                cn = c + y
-                dfs(rn,cn)
-                
-        ans = 0
-        for i in range (rows):
-            for j in range (cols):               
+                nx, ny = r + x, c + y
+                dfs(nx,ny)
+
+        result = 0
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1":
-                    ans += 1
-                    dfs(i,j)                
-        return ans
+                    result += 1
+                    dfs(i,j)
+        return result
 
-        
-
-
-
-
-
+        #keep a global variable that counts the island
+        #return that variable
