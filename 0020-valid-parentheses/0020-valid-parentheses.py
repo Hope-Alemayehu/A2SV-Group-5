e7a1d70a48f6ed5d54a352b55e0aefd8afa7e6c0
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-12 20:31:44 +0300

    Time: 0 ms (100%), Space: 16.6 MB (78.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,18 +5,16 @@ class Solution:
         #create a stack
         stack = []
         #create a map that connect the opening and closing brackets
-        bracket_map = {")":"(", "]":"[","}":"{"}
+        bracket_pairs = {")":"(", "]":"[","}":"{"}
 
         #iterate through the string
-        N = len(s)
-        for i in range(N):
+        for char in s:
             #check if the stack isn't empty 
             #the last element is the opeing bracket of current string
-            if stack and s[i] in bracket_map and bracket_map[s[i]] == stack[-1]:
-                #if that's the case pop it 
+            if stack and char in bracket_pairs and bracket_pairs[char] == stack[-1]:
                 stack.pop()
                 continue
             #if not we append it to the stack to find its partner
-            stack.append(s[i])
+            stack.append(char)
         return len(stack) == 0
     
