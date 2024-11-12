b1c047e5e31cd72fa25d0c061ee45549b6024133
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-13 01:38:38 +0300

    Time: 39 ms (99.82%), Space: 28.9 MB (99.46%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
@@ -1,14 +1,23 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        N = len(arr) 
-        minDiff = float("inf")
+        
+        #sort the array
         arr.sort()
-        ans = []
-
-        for i in range(N-1):
-            minDiff = min(minDiff, arr[i+1] - arr[i])
-
-        for i in range(N - 1):
-            if arr[i+1] - arr[i] == minDiff:
-                ans.append([arr[i],arr[i+1]])
-        return ans
+        #min_distance variable
+        min_distance = arr[1] - arr[0]
+        #create a result list
+        result = [[arr[0],arr[1]]]
+        #start iterating till len(arr) - 1
+        for i in range(1,len(arr)-1):
+            a, b = arr[i], arr[i+1]
+            distance = b - a 
+            
+            if distance == min_distance:
+                result.append([a,b])
+            elif distance < min_distance:
+                min_distance = distance
+                result.clear()
+                result.append([a,b])
+            else:
+                continue
+        return result
