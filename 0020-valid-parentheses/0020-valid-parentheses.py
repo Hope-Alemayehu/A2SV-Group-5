7ca8bdb1ac7abf7d4345d1cb1096a752c6ef6a25
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-12 20:27:59 +0300

    Time: 0 ms (100%), Space: 16.5 MB (78.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         #time complexity O(N)
-        #space complexity O(N)
-        bracket_map = {"}":"{",")":"(","]":"["}
+        #time complexity O(N) + O(3) ~ O(N)
+        #create a stack
         stack = []
+        #create a map that connect the opening and closing brackets
+        bracket_map = {")":"(", "]":"[","}":"{"}
 
-        for c in s:
-            if c in bracket_map and stack and stack[-1] == bracket_map[c]:
+        #iterate through the string
+        N = len(s)
+        for i in range(N):
+            #check if the stack isn't empty 
+            #the last element is the opeing bracket of current string
+            if stack and s[i] in bracket_map and bracket_map[s[i]] == stack[-1]:
+                #if that's the case pop it 
                 stack.pop()
                 continue
-            stack.append(c)
+            #if not we append it to the stack to find its partner
+            stack.append(s[i])
         return len(stack) == 0
-
-        
+    
