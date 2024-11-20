e8be4836f1080984a8da0f1b33be1c75c8cf78b2
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-11-20 10:52:04 +0300

    Time: 71 ms (70.51%), Space: 26 MB (81.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        #time complexity O(N)
+        #space complexity O(1)
+        min_price = float("inf")
+        max_profit = 0
 
-        #brute force approch
-        N = len(prices)
-        # for i in range(N - 1):
-        #     for j in range(i+1,N):
-        #         profit = max(profit, (prices[j] - prices[i]))
-        # return profit
-
-        #optimized approch
-        buy, sell = 0, 1
-        while buy < sell and sell < N:
-            cur_profit = prices[sell] - prices[buy]
-            if cur_profit < 0:
-                buy = sell
-                sell += 1
-                continue
-            profit = max(cur_profit, profit)
-            sell += 1
-        return profit
-            
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+        return max_profit
