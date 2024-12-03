3f0153a6f913aeaf8eac932b0691d56329593bde
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2024-12-03 13:30:22 +0300

    Time: 11 ms (81.24%), Space: 18 MB (7.79%) - LeetHub

--- 1272-invalid-transactions/1272-invalid-transactions.py
+++ 1272-invalid-transactions/1272-invalid-transactions.py
@@ -1,20 +1,29 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        invalid = set()
-        nameMap = defaultdict(list)
-        
+        #time complexity O(NlogN + O(N*K))
+        #space complexity O(N)
+        parser = []
+        n = len(transactions)
         for i in range(len(transactions)):
-            name,time,amount,city = transactions[i].split(",")
-            if int(amount) > 1000:
-                invalid.add(i)
-            
-            for prevtime, prevcity, index in nameMap[name]:
-                if  abs(prevtime - int(time)) <= 60 and prevcity != city:
-                    invalid.add(i)
+            name, time, amount, city = transactions[i].split(",")
+            parser.append([name,int(time),int(amount),city, i])
+        parser.sort(key=lambda x : (x[0],x[1]))
+        invalid = set()
+        for i, (name, time, amount, city, index) in enumerate(parser):
+            if amount > 1000:
+                invalid.add(index)
+                
+            for j in range(i-1,-1,-1):
+                if parser[j][0] != name:
+                    break
+                if time - parser[j][1] > 60:
+                    break
+                if city != parser[j][3]:
                     invalid.add(index)
-            nameMap[name].append([int(time),city,i])
-
+                    invalid.add(parser[j][4])
         result = []
-        for index in invalid:
-            result.append(transactions[index])
+        for idx in invalid:
+            result.append(transactions[idx])
         return result
+            
+        
